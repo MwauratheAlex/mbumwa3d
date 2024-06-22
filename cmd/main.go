fbf46c77ddd455924523ace9cdc8927420212957
Mbugua <mwaurathealex@gmail.com>
2024-06-22 13:29:30 +0300

    load env

--- cmd/main.go
+++ cmd/main.go
@@ -2,18 +2,25 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/mwaurathealex/mbumwa3d/internal/handlers"
+	"github.com/mwaurathealex/mbumwa3d/internal/initializers"
 )
 
+func init() {
+	initializers.LoadEnvVariables()
+}
+
 func main() {
+	port := os.Getenv("PORT")
 	r := chi.NewMux()
 
 	r.Handle("/*", public())
 
 	r.Get("/", handlers.Make(handlers.HandleHome))
-	http.ListenAndServe(":3000", r)
+	http.ListenAndServe(port, r)
 }
 
 func public() http.Handler {
--- go.mod
+++ go.mod
@@ -5,4 +5,5 @@ go 1.22.0
 require (
 	github.com/a-h/templ v0.2.707 // indirect
 	github.com/go-chi/chi/v5 v5.0.14 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 )
