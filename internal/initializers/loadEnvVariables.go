b1696c151b939cd0ce4a2a5593f21d9701d7628f
Mbugua <mwaurathealex@gmail.com>
2024-07-07 16:02:43 +0300

    check if prod before loading .env

--- internal/initializers/loadEnvVariables.go
+++ internal/initializers/loadEnvVariables.go
@@ -3,11 +3,15 @@ package initializers
 import (
 	"github.com/joho/godotenv"
 	"log"
+	"os"
 )
 
 func LoadEnvVariables() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file", err)
+	// Check if running in production environment
+	if os.Getenv("RAILWAY_ENVIRONMENT") == "" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Fatal("Error loading .env file", err)
+		}
 	}
 }
